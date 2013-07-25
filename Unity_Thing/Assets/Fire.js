f15702604253bf654bd7cbcf5a192b59f348b29b
JKK <ursinewalrus@yahoo.com>
2013-07-25 13:50:19 -0400

    stuff

---
+++ Unity_Thing/Assets/Fire.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+/*public var doomspew : ParticleSystem;
+Invoke("goingdown",5);
+function Start() {
+	doomspew = GameObject.Find("Doom").GetComponent(ParticleSystem);
+}
+
+function goingdown(){
+	doomspew.Play;
+}*/
+var doomspew : ParticleSystem;
+function Start() {
+	doomspew = GameObject.Find("Doom").GetComponent(ParticleSystem);
+}
+//Invoke("goingdown",5);
+
+setTimeout(function () {
+	doomspew.Emit(40);
+	
+},400);
