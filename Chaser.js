aca398a9ce4fda5603909be69149376dcce9f0a3
JKK <ursinewalrus@yahoo.com>
2013-07-25 13:37:26 -0400

    dunno if itll work

---
+++ Chaser.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+Invoke("Dropper", 5);
+Invoke("Drama", 1);
+    
+function Dropper () {
+    	//rigidbody.useGravity = true;
+    rigidbody.AddForce(5*Physics.gravity);
+}
+function Drama(){
+  // ParticleEmitter.emit = true;
+}
+
+
---
+++ Fire.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+/*public var doomspew : ParticleSystem;
+Invoke("goingdown",5);
+function Start() {
+	doomspew = GameObject.Find("Doom").GetComponent(ParticleSystem);
+}
+
+function goingdown(){
+	doomspew.Play;
+}*/
+var doomspew : ParticleSystem;
+function Start() {
+	doomspew = GameObject.Find("Doom").GetComponent(ParticleSystem);
+}
+Invoke("goingdown",5);
+
+function goingdown(){
+	doomspew.Emit = true;
+}
